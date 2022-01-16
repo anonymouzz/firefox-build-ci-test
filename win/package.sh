2ab65621fb60a575a371049d73bc72eaafc0ecbe
anonymouzz <sokolov.m.v@gmail.com>
2022-01-16 17:40:15 +0700

    jenkinsfile.win: fix typos

--- win/package.sh
+++ win/package.sh
@@ -4,4 +4,4 @@ DIST="/c/jenkins/firefox-win/dist"
 mkdir -p ${DIST}
 
 cd ${ROOT} && ./mach --no-interactive package
-cp ${ROOT}/obj-x86_64-pc-mingw32/firefox*.zip ${DIST}/
+cp ${ROOT}/obj-x86_64-pc-mingw32/dist/firefox*.zip ${DIST}/
