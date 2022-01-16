3448eeac3fa313f12ce444a7a09ee20ec8880d6e
anonymouzz <sokolov.m.v@gmail.com>
2022-01-16 17:25:51 +0700

    jenkinsfile.win: fix typos

--- win/package.sh
+++ win/package.sh
@@ -1,1 +1,7 @@
-cd /c/mozilla-source/mozilla-unified && ./mach --no-interactive package
+ROOT="/c/mozilla-source/mozilla-unified"
+DIST="/c/jenkins/firefox-win/dist"
+
+mkdir -p ${DIST}
+
+cd ${ROOT} && ./mach --no-interactive package
+cp ${ROOT}/obj-x86_64-pc-mingw32/firefox*.zip ${DIST}/
